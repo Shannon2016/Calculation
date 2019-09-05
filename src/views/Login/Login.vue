e2193c771b22ae7fa1a4e45c444e0c2027bb6872
897811139@qq.com <31436943+Azureli@users.noreply.github.com>
2019-09-05 10:15:21 +0800

    login css

--- src/views/Login/Login.vue
+++ src/views/Login/Login.vue
@@ -1,14 +1,19 @@
 <template>
   <LoginFrame>
-    <div slot='top'>
+
+    <div slot='top' >
         <div class="title">欢迎使用</div>
         <div class="title">工程认证指标点系统</div>
     </div>
+
     <div slot='center'>
-      <el-input id="username" v-model="username"></el-input>
-      <el-input id="password" v-model="password"></el-input>
-      <el-button @click="onLogin">登录</el-button>
+    <div class="card">
+        <el-input class="text" id="username" v-model="username" prefix-icon="el-icon-user"></el-input>
+        <el-input class="text" id="password" v-model="password" prefix-icon="el-icon-unlock"></el-input>
+        <el-button  class="button"   @click="onLogin">登录</el-button>
+     </div>
     </div>
+
   </LoginFrame>
 </template>
 <script>
@@ -48,5 +53,41 @@ export default {
 }
 </script>
 <style>
-
+.el-input__inner {
+      background-color: transparent !important;
+    }
+</style>
+<style scoped>
+  .card{
+    padding: 10%;
+    height: 50%;
+    background-color: transparent;
+  }
+   .card :hover{
+    background-color: transparent;
+    opacity: 0.8;
+    transition-duration: 0.5s;
+    transform: scale3d(1.02,1.02,1);
+  }
+   .title {
+        width: 100%;
+        justify-content: center;
+        color: white;
+        font-size: 30px;
+        font-family: 'Microsoft YaHei';
+    }
+    .text{
+      margin-top: 5%;
+      opacity: 0.8;
+      background-color: transparent !important;
+    }
+    .button{
+      margin-top: 10%;
+      width: 80%;
+      background-color:transparent;
+      font-family: 'Microsoft YaHei';
+      font-size: 20px;
+      color:darkblue;
+    }
+    
 </style>
