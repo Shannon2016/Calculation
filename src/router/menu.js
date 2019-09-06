f6a29c65d732fa2e82c66d7e6710e5b1267bc970
897811139@qq.com <31436943+Azureli@users.noreply.github.com>
2019-09-06 16:17:29 +0800

    icon

--- src/router/menu.js
+++ src/router/menu.js
@@ -28,17 +28,17 @@ export default {
           path: '/uploadTeacher',
           name: 'uploadTeacher',
           cn: '上传教师信息',
-          icon: 'el-icon-data-analysis'
+          
         }, {
           path: '/uploadCourse',
           name: 'uploadCourse',
           cn: '上传课程信息',
-          icon: 'el-icon-notebook-1'
+         
         }, {
           path: '/uploadStudent',
           name: 'uploadStudent',
           cn: '上传学生选课信息',
-          icon: 'el-icon-thumb'
+          
         }
       ]
     },
@@ -46,13 +46,13 @@ export default {
       path: '/studentEvaluate',
       name: 'StudentEvaluate',
       cn: '学生评价',
-      icon: 'el-icon-folder',
+      icon: 'el-icon-chat-dot-square',
       child: [
         {
           path: '/studentEvaluate',
           name: 'studentEvaluate',
           cn: '本期评价',
-          icon: 'el-icon-folder'
+          
         }
       ]
     }
